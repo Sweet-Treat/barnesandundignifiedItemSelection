28902170e834f48cea6290836c6e8a1b0f839c1d
Matteo Besio <matteo.besio@gmail.com>
2020-11-10 21:09:42 -0800

    setup react and webpack

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const port = 3001;
 // serve the static files
 
 //create a test hello world response to the root endpoint to check if everything works properly
+
+app.use(express.static('client/dist'))
+
 app.get('/', (req, res) => {
   res.send('Hello, world');
 })
