787d3a937b4a7857349fcf2f560709e8943a6897
Matteo Besio <matteo.besio@gmail.com>
2020-11-11 21:06:32 -0800

    completed api endpoint to get book info

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
 
+const Books = require('../db/index.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // serve the static files
 
 //create a test hello world response to the root endpoint to check if everything works properly
 
 app.use(express.static('client/dist'))
+
+app.get('/:id', (req, res) => {
+  var id = req.params.id;
+  Books.findOne({isbn: id}, (err, bookInfo) => {
+    if (err) {
+      console.log('Book does not exist')
+      res.send();
+    } else {
+      console.log('Book exists')
+      res.send(bookInfo);
+    }
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world');
