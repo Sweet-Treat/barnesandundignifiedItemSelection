762385f898a3c08164311e91fbb2e4b7066dd625
Matteo Besio <matteo.besio@gmail.com>
2020-11-12 18:34:18 -0800

    installed jest dependency and run first test

---
+++ spec/test.js
@@ -0,0 +1,11 @@
+describe('Test for tests', () => {
+  it('should be true', () => {
+    const foo = true;
+    expect(foo).toBe(true);
+  });
+  it('should be false', () => {
+    const boo = false;
+    expect(boo).toBe(false);
+  })
+
+});
