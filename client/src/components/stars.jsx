48ed0bc866fd099a8a6da7946cae3b24d0d9b75a
Matteo Besio <matteo.besio@gmail.com>
2020-11-13 21:24:10 -0800

    first attempt to stars component to be improved

--- client/src/components/stars.jsx
+++ client/src/components/stars.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { IoIosStar, IoIosStarHalf } from 'react-icons/Io';
+
 
 class Stars extends React.Component {
   constructor(props) {
     super (props)
     this.state = {
-      rating: ''
+      rating: 3.3
     }
   }
   render() {
     return(
-      <span>Hello from Stars</span>
+      <span>
+        { this.state.rating < 0.25 && <span> <IoIosStar className="star-empty"/></span>} { this.state.rating >= 0.25 && this.state.rating < 0.75 && <span> <IoIosStarHalf className="star" /> </span>} {this.state.rating >= 0.75 && <span> <IoIosStar className="star" /> </span>}
+
+        { this.state.rating < 1.25 && <span> <IoIosStar className="star-empty"/></span>} { this.state.rating >= 1.25 &&  this.state.rating < 1.75 && <span> <IoIosStarHalf className="star" /> </span>} {this.state.rating >= 1.75 && <span> <IoIosStar className="star" /> </span>}
+
+        { this.state.rating < 2.25 && <span> <IoIosStar className="star-empty"/></span>} { this.state.rating >= 2.25 &&  this.state.rating < 2.75 && <span> <IoIosStarHalf className="star" /> </span>} {this.state.rating >= 2.75 && <span> <IoIosStar className="star" /> </span>}
+
+        { this.state.rating < 3.25 && <span> <IoIosStar className="star-empty"/></span>} { this.state.rating >= 3.25 && this.state.rating < 3.75 && <span> <IoIosStarHalf className="star" /> </span>} {this.state.rating >= 3.75 && <span> <IoIosStar className="star" /> </span>}
+
+        { this.state.rating < 4.25 && <span> <IoIosStar className="star-empty"/></span>} { this.state.rating >= 4.25 && this.state.rating < 4.75 && <span> <IoIosStarHalf className="star" /> </span>} {this.state.rating >= 4.75 && <span> <IoIosStar className="star" /> </span>}
+      </span>
     );
   }
 }
