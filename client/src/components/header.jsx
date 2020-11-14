5910835ea87e1514aa70773c781e3ff4a5a110a0
Matteo Besio <matteo.besio@gmail.com>
2020-11-13 19:47:02 -0800

    completed barebones header component and left placeholder for stars component

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -5,10 +5,13 @@ var Header = () => {
     <div>
       <div className="book-title"> book title </div>
       <div>
-        <span>by</span>
+        <span className="book-author">by</span>
         <span className="book-author green-text"> FirstName LastName </span>
       </div>
-      <div> stars will be here </div> <div> average nd number will be here </div>
+      <div>
+        <span> stars Component </span>
+        <span className="rating green-text"> average rating | numb reviews </span>
+      </div>
     </div>
   );
 }
