20f3785b52620044aba1e77ee8183b61bb2eb414
Matteo Besio <matteo.besio@gmail.com>
2020-11-13 18:58:31 -0800

    made App stateful

--- client/src/components/trial.jsx
+++ client/src/components/trial.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 var Trial = () => (
-  <div>Hello from React from Trial</div>
+  <div>Hello from React from Trial again</div>
 )
 
 export default Trial;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Trial from './components/trial.jsx';
 
-var App = () => (
-  <div><Trial/></div>
-)
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div><Trial/></div>
+    );
+  }
+}
 
 ReactDOM.render(<App/>, document.getElementById('root'));
