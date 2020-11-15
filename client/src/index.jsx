694a5bf8fd1599d1d85459cc59d559839f73f5ff
Matteo Besio <matteo.besio@gmail.com>
2020-11-14 20:31:58 -0800

    added hardcoded endpoints for product ans reviews

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,27 +8,59 @@ class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      bookInfo: {}
+      titleAndAuthor: {},
+      reviews: {},
+      inventory: {},
     }
-    this.getBookInfo = this.getBookInfo.bind(this);
+    this.getInventory = this.getInventory.bind(this);
+    this.getTitleAndAuthor = this.getTitleAndAuthor.bind(this);
+    this.getReviews = this.getReviews.bind(this);
   }
 
-  getBookInfo(isbn) {
-    console.log('isbn ', isbn)
+  getInventory(isbn) {
     axios({
       method: 'get',
-      url: `/${isbn}`
+      url: `/getInventory/${isbn}`
     })
     .then((data) => {
-      console.log('this is what came back: data ', data);
+      console.log('this is invetory data ', data);
     })
     .catch((err) => {
-      console.log('there was an error during the axios get request: err ', err)
+      console.log('there was an error during the axios inventory get request: err ', err)
+    })
+  }
+
+  getTitleAndAuthor(isbn) {
+    axios({
+      method: 'get',
+      url: `/product/${isbn}`
+    })
+    .then((data) => {
+      console.log('this is product data ', data);
+    })
+    .catch((err) => {
+      console.log('there was an error during the axios title get request: err ', err)
+    })
+  }
+
+  getReviews(isbn) {
+    axios({
+      method: 'get',
+      url: `/reviewssummary/${isbn}`
+    })
+    .then((data) => {
+      console.log('this is reviews data ', data);
+    })
+    .catch((err) => {
+      console.log('there was an error during the axios reviews get request: err ', err)
     })
   }
 
   componentDidMount() {
-    this.getBookInfo(259);
+    var isbn = 10;
+    this.getInventory(isbn);
+    this.getTitleAndAuthor(isbn);
+    this.getReviews(isbn);
   }
 
 
--- server/index.js
+++ server/index.js
@@ -14,19 +14,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('client/dist'))
 
-app.get('/:id', (req, res) => {
+app.get('/getInventory/:id', (req, res) => {
   var id = req.params.id || 1;
   Books.findOne({isbn: id}, (err, bookInfo) => {
     if (err) {
-      console.log('Book does not exist')
+      console.log('ISBN inventory does not exist')
       res.send();
     } else {
-      console.log('Book exists')
+      console.log('ISBN inventory exists')
      // console.log('bookInfo', bookInfo)
       res.send(bookInfo);
     }
   })
+});
+
+// hard coding what the product endpoint should return
+app.get('/product/:id', (req, res) => {
+  var product = {
+    title: 'Rhythm of War (Stormlight Archive Series #4)',
+    author: 'Brandon Sanderson'
+  }
+  res.send(product);
 })
+
+// hard coding what the reviews endpoint should return
+app.get('/reviewssummary/:id', (req, res) => {
+  var reviews = {
+    totalReviews: 10,
+    avgRating: 3.5
+  }
+  res.send(reviews);
+})
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world');
