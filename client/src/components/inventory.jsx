c07d6ddad1b351f376e22d91cd4eed523b5d69f5
Matteo Besio <matteo.besio@gmail.com>
2020-11-15 11:38:21 -0800

    refactored inventory to be a stateful component

--- client/src/components/inventory.jsx
+++ client/src/components/inventory.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-var Inventory = () => {
-  return (
-    <div>This is an inventory component that I will start building</div>
-  )
+class Inventory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPrice: 10,
+      currentDiscount: 5
+    }
+  }
+
+  render() {
+    return (
+      <div>This is an inventory component that I will start building</div>
+    )
+  }
+
 }
 
 export default Inventory;
