c7c65537979a97a40638f82fce08921ae312387a
Matteo Besio <matteo.besio@gmail.com>
2020-11-15 19:31:14 -0800

    added styling to radiobuttons component

--- client/src/components/radiobuttons.jsx
+++ client/src/components/radiobuttons.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { AiOutlineInfoCircle } from 'react-icons/Ai';
 
 var Radiobuttons = () => {
   return(
     <div>
-      <div>
-        <label>
-          <input type="radio" checked/>
+
+        <div>
+          <label className="radio-container">
+            <input type="radio" name="selection" checked/>
             <span>Ship This Item - </span>
-            <span>Qualifies for Free Shipping</span>
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="radio"/>
-            <span>Buy Online, Pick in Store at </span>
-            <span>B&N Bay Street - Emeryville</span>
-        </label>
-      </div>
+            <span className="radio-container-bold">Qualifies for Free Shipping </span>
+            <AiOutlineInfoCircle className="green-text circle-info"/>
+          </label>
+        </div>
+        <div>
+          <label className="radio-container">
+            <input type="radio" name="selection"/>
+            <span>Buy Online, Pick in Store at </span>            <span className="radio-container-bold">B&N Bay Street - Emeryville </span>
+            <AiOutlineInfoCircle className="green-text circle-info"/>
+          </label>
+          <div className="green-text availability">Check Availability at Nearby Stores</div>
+        </div>
+
     </div>
   )
 }
