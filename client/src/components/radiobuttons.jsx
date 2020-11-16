023f6e8b9f849e5b09eca86816a90290ab7cee80
Matteo Besio <matteo.besio@gmail.com>
2020-11-15 18:38:56 -0800

    rendered initial radio buttons

--- client/src/components/radiobuttons.jsx
+++ client/src/components/radiobuttons.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 var Radiobuttons = () => {
   return(
-    <div>Hello from the Radiobuttons component</div>
+    <div>
+      <div>
+        <label>
+          <input type="radio" checked/>
+            <span>Ship This Item - </span>
+            <span>Qualifies for Free Shipping</span>
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="radio"/>
+            <span>Buy Online, Pick in Store at </span>
+            <span>B&N Bay Street - Emeryville</span>
+        </label>
+      </div>
+    </div>
   )
 }
 
