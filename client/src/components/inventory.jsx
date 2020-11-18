e24dcebaf0ca1fe98e068e0e9b26a921c9ca563a
Matteo Besio <matteo.besio@gmail.com>
2020-11-17 20:23:12 -0800

    refactored inventory to recieve inventory props from App

--- client/src/components/inventory.jsx
+++ client/src/components/inventory.jsx
@@ -7,56 +7,24 @@ import Options from './options.jsx';
 class Inventory extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inventory: [],
-      currentOption: 0,
-      currentName: 'Placeholder name',
-      regularPrice: 999,
-      currentDiscount: 999
-    }
-    this.getInventory = this.getInventory.bind(this);
-  }
-
-  getInventory(isbn) {
-    axios({
-      method: 'get',
-      url: `/formats/${isbn}`
-    })
-    .then((data) => {
-      //console.log('this is inventory data ', data);
-      this.setState({
-        inventory: data.data.options,
-        currentName: data.data.options[0].name,
-        regularPrice: data.data.options[0].price,
-        currentDiscount: data.data.options[0].discount
-      })
-    })
-    .catch((err) => {
-      console.log('there was an error during the axios inventory get request: err ', err)
-    })
-  }
-
-  componentDidMount() {
-    var isbn = 16;
-    this.getInventory(isbn);
   }
 
   render() {
     return (
       <div>
-        <div className ="name-type"> {this.state.currentName} </div>
+        <div className ="name-type"> {this.props.currentName} </div>
         <div>
-          <span className ="price"> ${ this.state.currentDiscount === 0 ? `${this.state.regularPrice}.99` : ((this.state.regularPrice + 0.99) * (1 - this.state.currentDiscount / 100)).toFixed(2)} </span>
+          <span className ="price"> ${ this.props.currentDiscount === 0 ? `${this.props.regularPrice}.99` : ((this.props.regularPrice + 0.99) * (1 - this.props.currentDiscount / 100)).toFixed(2)} </span>
 
-          {this.state.currentDiscount !== 0 &&
+          {this.props.currentDiscount !== 0 &&
           <span>
-            <span className ="price-regular"> ${this.state.regularPrice}.99</span>
+            <span className ="price-regular"> ${this.props.regularPrice}.99</span>
             <span className ="price-pipe"> | </span>
 
-            <span className ="percentage-discount"> Save {this.state.currentDiscount}% </span>
+            <span className ="percentage-discount"> Save {this.props.currentDiscount}% </span>
           </span>}
           <div>
-            <Options inventory = {this.state.inventory} currentOption = {this.state.currentOption}/>
+            <Options inventory = {this.props.inventory} currentOption = {this.props.currentOption}/>
           </div>
         </div>
       </div>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,11 +15,36 @@ class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
+      inventory: [],
+      currentOption: 0,
+      currentName: 'Placeholder name',
+      regularPrice: 999,
+      currentDiscount: 999,
       titleAndAuthor: {},
       reviews: {}
     }
+    this.getInventory = this.getInventory.bind(this);
     this.getTitleAndAuthor = this.getTitleAndAuthor.bind(this);
     this.getReviews = this.getReviews.bind(this);
+  }
+
+  getInventory(isbn) {
+    axios({
+      method: 'get',
+      url: `/formats/${isbn}`
+    })
+    .then((data) => {
+      //console.log('this is inventory data ', data);
+      this.setState({
+        inventory: data.data.options,
+        currentName: data.data.options[0].name,
+        regularPrice: data.data.options[0].price,
+        currentDiscount: data.data.options[0].discount
+      })
+    })
+    .catch((err) => {
+      console.log('there was an error during the axios inventory get request: err ', err)
+    })
   }
 
   getTitleAndAuthor(isbn) {
@@ -56,6 +81,7 @@ class App extends React.Component {
 
   componentDidMount() {
     var isbn = 16;
+    this.getInventory(isbn);
     this.getTitleAndAuthor(isbn);
     this.getReviews(isbn);
   }
@@ -65,7 +91,7 @@ class App extends React.Component {
     return (
       <div>
         <div><Header titleAndAuthor = {this.state.titleAndAuthor} reviews = {this.state.reviews}/></div>
-        <div><Inventory/></div>
+        <div><Inventory inventory={this.state.inventory} currentOption={this.state.currentOption} currentName={this.state.currentName} regularPrice={this.state.regularPrice} currentDiscount={this.state.currentDiscount}/></div>
         <div><Radiobuttons/></div>
         <div><Footer/></div>
         <div><Trial/></div>
