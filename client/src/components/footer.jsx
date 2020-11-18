fe3816d0ea9605af09aacb7ab5b83e8965ea9734
Matteo Besio <matteo.besio@gmail.com>
2020-11-17 19:43:17 -0800

    completed footer for non Nook books

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { RiTruckLine } from 'react-icons/Ri';
 
 var Footer = () => {
   return (
-    <div>Hello from the Footer component</div>
+    <div>
+      <span><RiTruckLine className="footer-truck"/></span>
+      <span className="footer-text">Members save with free shipping everyday</span>
+      <div className="green-text footer-details">See details</div>
+    </div>
   )
 }
 
