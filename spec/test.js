75e7aad1689d09cb4b9bf62a332daa4bda3163e0
Matteo Besio <matteo.besio@gmail.com>
2020-11-17 23:30:52 -0800

    added unit testing for header component

--- spec/test.js
+++ spec/test.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+
 import Trial from '../client/src/components/trial.jsx'
+import Header from '../client/src/components/header.jsx'
+import Stars from '../client/src/components/stars.jsx'
+
 
 describe('Test for tests', () => {
   it('should be true', () => {
@@ -15,9 +19,32 @@ describe('Test for tests', () => {
 });
 
 
-describe('Trial test', () => {
-  it('should have text in a div', () => {
-    const wrapper = shallow(<Trial/>);
-    expect(wrapper.text()).toBe('Hello from React from Trial');
+// describe('Trial test', () => {
+//   it('should have text in a div', () => {
+//     const wrapper = shallow(<Trial/>);
+//     expect(wrapper.text()).toBe('Hello from React from Trial');
+//   })
+// })
+
+describe('Test for header component', () => {
+  var titleAndAuthor, reviews, wrapper;
+
+  beforeEach(() => {
+    titleAndAuthor = {
+      title: 'The Stranger',
+      author: 'Albert Camus'
+    };
+    reviews = {
+      avgRating: 4.97,
+      totalReviews: 348
+    };
+    wrapper = shallow(<Header titleAndAuthor={titleAndAuthor} reviews={reviews}/>);
+  });
+
+  it('a title should exist', () => {
+    expect(wrapper.find('.book-title')).toHaveLength(1);
   })
+  it('an author should exist', () => {
+    expect(wrapper.find('.book-author')).toHaveLength(2);
+  });
 })
