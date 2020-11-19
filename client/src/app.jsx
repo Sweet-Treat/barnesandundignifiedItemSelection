e1e582a1bc797ec399fc89b9d69b2aae0f64b82c
Matteo Besio <matteo.besio@gmail.com>
2020-11-18 19:29:38 -0800

    added tests for the inventory component

---
+++ client/src/app.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+
+import axios from 'axios';
+
+import Trial from './components/trial.jsx';
+import Header from './components/header.jsx';
+import Inventory from './components/inventory.jsx';
+import Radiobuttons from './components/radiobuttons.jsx';
+import Footer from './components/footer.jsx';
+
+
+
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      inventory: [],
+      currentOption: 0,
+      currentName: 'Placeholder name',
+      regularPrice: 999,
+      currentDiscount: 999,
+      titleAndAuthor: {},
+      reviews: {}
+    }
+    this.getInventory = this.getInventory.bind(this);
+    this.getTitleAndAuthor = this.getTitleAndAuthor.bind(this);
+    this.getReviews = this.getReviews.bind(this);
+    this.handleFormatClick = this.handleFormatClick.bind(this);
+  }
+
+  handleFormatClick(value) {
+    this.setState({
+      currentOption: value
+    })
+  }
+
+  getInventory(isbn) {
+    axios({
+      method: 'get',
+      url: `/product/${isbn}/formats`
+    })
+    .then((data) => {
+      //console.log('this is inventory data ', data);
+      this.setState({
+        inventory: data.data.formats,
+        currentName: data.data.formats[0].name,
+        regularPrice: data.data.formats[0].price,
+        currentDiscount: data.data.formats[0].discount
+      })
+    })
+    .catch((err) => {
+      console.log('there was an error during the axios inventory get request: err ', err)
+    })
+  }
+
+  getTitleAndAuthor(isbn) {
+    axios({
+      method: 'get',
+      url: `/product/${isbn}`
+    })
+    .then((data) => {
+      //console.log('this is product data ', data);
+      this.setState({
+        titleAndAuthor: data.data
+      })
+    })
+    .catch((err) => {
+      console.log('there was an error during the axios title get request: err ', err)
+    })
+  }
+
+  getReviews(isbn) {
+    axios({
+      method: 'get',
+      url: `/reviewssummary/${isbn}`
+    })
+    .then((data) => {
+      //console.log('this is reviews data ', data);
+      this.setState({
+        reviews: data.data
+      })
+    })
+    .catch((err) => {
+      console.log('there was an error during the axios reviews get request: err ', err)
+    })
+  }
+
+  componentDidMount() {
+    var isbn = '16';
+    this.getInventory(isbn);
+    this.getTitleAndAuthor(isbn);
+    this.getReviews(isbn);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div><Header titleAndAuthor = {this.state.titleAndAuthor} reviews = {this.state.reviews}/></div>
+        <div><Inventory inventory={this.state.inventory} currentOption={this.state.currentOption} currentName={this.state.currentName} regularPrice={this.state.regularPrice} currentDiscount={this.state.currentDiscount} handleFormatClick={this.handleFormatClick}/></div>
+        <div><Radiobuttons/></div>
+        <div><Footer currentOption={this.state.currentOption}/></div>
+        <div><Trial/></div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- spec/test.js
+++ spec/test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import { spy } from 'sinon';
 
+//import App from '../client/src/app.jsx'
 import Trial from '../client/src/components/trial.jsx'
 import Header from '../client/src/components/header.jsx'
-import Stars from '../client/src/components/stars.jsx'
+//import Stars from '../client/src/components/stars.jsx'
+import Inventory from '../client/src/components/inventory.jsx'
 
 
 describe('Test for tests', () => {
@@ -26,6 +29,17 @@ describe('Test for tests', () => {
 //   })
 // })
 
+// test for the App Component
+// describe('The App component render all the child components', () => {
+//   it('should have a Header Component' , () => {
+//     const wrapper = mount(<App/>);
+//     expect(wrapper.find(Header).length).toEqual(1);
+//   })
+
+// })
+
+
+
 describe('Test for header component', () => {
   var titleAndAuthor, reviews, wrapper;
 
@@ -47,4 +61,46 @@ describe('Test for header component', () => {
   it('an author should exist', () => {
     expect(wrapper.find('.book-author')).toHaveLength(2);
   });
+})
+
+
+// inventory component - should have name, price and discount
+describe('Test the inventory component', () => {
+  var inventory, currentOption, currentName, regularPrice, currentDiscount, handleFormatClick, wrapper;
+
+  beforeEach(() => {
+    inventory = [
+      {
+        name: 'Hardcover',
+        price: 31,
+        discount: 5,
+        buyOnlinePickUpInStore: true
+      },
+      {
+        name: "Nook Book",
+        price: 7,
+        discount: 8,
+        buyOnlinePickUpInStore: false
+      }
+    ];
+    currentOption = 0;
+    currentName = 'Hardcover';
+    regularPrice = 31;
+    currentDiscount = 5;
+    handleFormatClick = spy();
+
+    wrapper = shallow(<Inventory inventory={inventory} currentOption={currentOption} currentName={currentName} regularPrice = {regularPrice} currentDiscount={currentDiscount} handleFormatClick={handleFormatClick}/>);
+  });
+
+  it('it should have a price', () => {
+    expect(wrapper.find('.price')).toHaveLength(1);
+  })
+
+  it('it should have a price before discount', () => {
+    expect(wrapper.find('.price-regular')).toHaveLength(1);
+  })
+
+  it('it should have a discount percentage', () => {
+    expect(wrapper.find('.percentage-discount')).toHaveLength(1);
+  })
 })
