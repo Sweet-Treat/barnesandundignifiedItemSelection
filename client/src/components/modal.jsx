14cd90c6585d9e65ed7300865dfe095ba349aedb
Matteo Besio <matteo.besio@gmail.com>
2020-11-24 12:05:14 -0800

    refactored Modal to a class component

--- client/src/components/modal.jsx
+++ client/src/components/modal.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
-var Modal = (props) => {
-  return (
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentFormat: ''
+    }
+  }
+
+  render(){
+    return(
     <div className='modal-main'>
       <div>
         <span className="modal-title"> All Formats & Editions </span>
-        <button onClick={props.handleAllInventoryClick}>x</button>
+        <button onClick={this.props.handleAllInventoryClick}>x</button>
       </div>
     </div>
-  )
+    )
+  }
 }
 
 export default Modal;
