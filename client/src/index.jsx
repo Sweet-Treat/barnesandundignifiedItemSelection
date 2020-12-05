9c1129cb98bbe56bc18f96c652cea861f8f6685c
Matteo Besio <matteo.besio@gmail.com>
2020-12-05 11:47:32 -0800

    changed dom id from root to item-selector and added unique id to all css elements

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './app.jsx';
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('item-selection'));
