80e9b5a28c4326a5d3e7d0a182a3414281f4d6ae
Matteo Besio <matteo.besio@gmail.com>
2020-12-21 19:26:51 -0800

    fixed remaining reference to localhost

--- login-mongo.sh
+++ login-mongo.sh
@@ -1,1 +1,1 @@
-ssh -i "/Users/matteo/Desktop/HackReactor/Immersive/front-end-capstone/service/fec-capstone.pem" ec2-user@ec2-3-138-201-221.us-east-2.compute.amazonaws.com
+ssh -i "/Users/matteo/Desktop/HackReactor/Immersive/front-end-capstone/service/fec-capstone.pem" ec2-user@ec2-3-140-191-190.us-east-2.compute.amazonaws.com
