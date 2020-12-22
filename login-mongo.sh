b091378f57923f7969f78ca10467a188b7007d49
Matteo Besio <matteo.besio@gmail.com>
2020-12-21 17:59:24 -0800

    add ec2 bash login files

--- login-mongo.sh
+++ login-mongo.sh
@@ -1,12 +1,1 @@
 ssh -i "/Users/matteo/Desktop/HackReactor/Immersive/front-end-capstone/service/fec-capstone.pem" ec2-user@ec2-3-138-201-221.us-east-2.compute.amazonaws.com
-
-
-# User admin for mongo
-# user: adminMatteo
-# pwd: subaL38k
-
-# User for mongo fecItemSelection database
-# user: itemSelectionOwner
-# pwd: abc123f
-
-# this is how the url to connect to mongo looks like --> 'mongodb://itemSelectionOwner:abc123f@ec2-3-138-201-221.us-east-2.compute.amazonaws.com/fecItemSelection'
