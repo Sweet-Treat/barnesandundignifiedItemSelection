d08a67a603d1089a933d81063b8cd5289a464a0e
Matteo Besio <matteo.besio@gmail.com>
2021-01-10 13:51:41 -0800

    version working locally

--- client/src/lib/getInventory.js
+++ client/src/lib/getInventory.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 var getInventory = (isbn, cb) => {
-  axios.get(`http://18.188.228.195/product/${isbn}/formats`)
+  axios.get(`/product/${isbn}/formats`)
   .then((data) => cb(null, data))
   .catch((err) => cb(err))
 }
